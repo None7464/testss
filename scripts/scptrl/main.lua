e5fdcc02552b20bae3e60bbb5c8c0aa8817a3305
None7464 <107632808+None7464@users.noreply.github.com>
2025-04-05 22:53:05 +0800

    Update main.lua

--- scripts/scptrl/main.lua
+++ scripts/scptrl/main.lua
@@ -1,1 +1,15 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/None7464/testss/main/Ui-Lib/Gui.lua", true))()
 
+local UI = library:CreateWindow({ text = "The Red Lake Script" })
+
+UI:AddToggle("Public Server Version", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/None7464/testss/main/scripts/scptrl/public.lua", true))()
+    wait()
+    library:DestroyUI()
+end)
+
+UI:AddToggle("Private Server Version", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/None7464/testss/main/scripts/scptrl/ps.lua", true))()
+    wait()
+    library:DestroyUI()
+end)
