37fe96778e193b2ea423530d70b328ff027d2a14
None7464 <107632808+None7464@users.noreply.github.com>
2025-04-05 12:18:12 +0800

    Update loader.lua

--- scripts/loader.lua
+++ scripts/loader.lua
@@ -1,3 +1,7 @@
 if game.GameId == 5766084948 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/None7464/testss/main/scripts/scptrl/main.lua", true))()
 end
+
+if game.GameId == 5766084948 then -- env checker soon
+    print("W.I.P")
+end
