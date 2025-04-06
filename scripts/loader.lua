349d5614601be32488625556bf681f4b284b6312
None7464 <107632808+None7464@users.noreply.github.com>
2025-04-07 01:00:05 +0800

    Update loader.lua

--- scripts/loader.lua
+++ scripts/loader.lua
@@ -1,4 +1,4 @@
 if game.GameId == 5766084948 then
     getgenv().__LOADER_USED = true
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/None7464/testss/main/scripts/scptrl/main.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/None7464/testss/main/scripts/scptrl/main.lua"))()
 end
