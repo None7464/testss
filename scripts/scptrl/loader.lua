bd1f9fbac1f1b96cfeeda4da5f2ee5a3d3a8923a
None7464 <107632808+None7464@users.noreply.github.com>
2025-04-16 02:07:28 +0800

    Update loader.lua

--- scripts/scptrl/loader.lua
+++ scripts/scptrl/loader.lua
@@ -1,42 +1,50 @@
 local function load()
-	local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/None7464/testss/main/Ui-Lib/Gui.lua", true))()
-	local privateScript = game:HttpGet("https://raw.githubusercontent.com/None7464/testss/main/scripts/scptrl/ps.lua", true)
-	local publicScript = game:HttpGet("https://raw.githubusercontent.com/None7464/testss/main/scripts/scptrl/public.lua", true)
-	
-	if _G.StopAutoExecute then
-		library.settings.last_used_script = nil
-		library:SaveSettings()
-	end
-	
-	library:LoadSettings()
-	local savedScript = library.settings.last_used_script
-	
-	
-	if not _G.StopAutoExecute then -- auto execute
-		if savedScript == "private" then
-			loadstring(privateScript)()
-			getgenv().__LOADER_USED = true
-		elseif savedScript == "public" then
-			loadstring(publicScript)()
-			getgenv().__LOADER_USED = true
-		end
-	end
-	
-	local UI = library:CreateWindow({ text = "The Red Lake Script" })
-	
-	UI:AddButton("Public Server Version", function()
-		loadstring(publicScript)()
-		library.settings.last_used_script = "public"
-		library:SaveSettings()
-		library:DestroyUI()
-	end)
-	
-	UI:AddButton("Private Server Version", function()
-		loadstring(privateScript)()
-		library.settings.last_used_script = "private"
-		library:SaveSettings()
-		library:DestroyUI()
-	end)
+    local library =
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/None7464/testss/main/Ui-Lib/Gui.lua", true))()
+    local privateScript =
+        game:HttpGet("https://raw.githubusercontent.com/None7464/testss/main/scripts/scptrl/ps.lua", true)
+    local publicScript =
+        game:HttpGet("https://raw.githubusercontent.com/None7464/testss/main/scripts/scptrl/public.lua", true)
+
+    if _G.StopAutoExecute then
+        library.settings.last_used_script = nil
+        library:SaveSettings()
+    end
+
+    library:LoadSettings()
+    local savedScript = library.settings.last_used_script
+
+    if not _G.StopAutoExecute then -- auto execute
+        if savedScript == "private" then
+            loadstring(privateScript)()
+            getgenv().__LOADER_USED = true
+        elseif savedScript == "public" then
+            loadstring(publicScript)()
+            getgenv().__LOADER_USED = true
+        end
+    end
+
+    local UI = library:CreateWindow({text = "The Red Lake Script"})
+
+    UI:AddButton(
+        "Public Server Version",
+        function()
+            loadstring(publicScript)()
+            library.settings.last_used_script = "public"
+            library:SaveSettings()
+            library:DestroyUI()
+        end
+    )
+
+    UI:AddButton(
+        "Private Server Version",
+        function()
+            loadstring(privateScript)()
+            library.settings.last_used_script = "private"
+            library:SaveSettings()
+            library:DestroyUI()
+        end
+    )
 end
 
 local HttpService = game:GetService("HttpService")
@@ -45,77 +53,81 @@ local function copyToClipboard(text)
     setclipboard(text)
 end
 
-local Notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/None7464/testss/main/Ui-Lib/Notification.lua", true))()
+local Notify =
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/None7464/testss/main/Ui-Lib/Notification.lua", true))()
 
 local function checkExecutor()
-	local issues = {}
+    local issues = {}
 
-	local Functions = {
-		["getrawmetatable"] = getrawmetatable or get_raw_metatable,
-		["setrawmetatable"] = setrawmetatable or set_raw_metatable,
-		["setreadonly"] = setreadonly or make_readonly or makereadonly,
-		["iswriteable"] = iswriteable or writeable or is_writeable,
+    local Functions = {
+        ["getrawmetatable"] = getrawmetatable or get_raw_metatable,
+        ["setrawmetatable"] = setrawmetatable or set_raw_metatable,
+        ["setreadonly"] = setreadonly or make_readonly or makereadonly,
+        ["iswriteable"] = iswriteable or writeable or is_writeable,
+        -- // IO Functions \--
+        ["isfolder"] = isfolder or syn_isfolder or is_folder,
+        ["isfile"] = isfile or syn_isfile or is_file,
+        ["delfolder"] = delfolder or syn_delfolder or del_folder,
+        ["delfile"] = delfile or syn_delfile or del_file,
+        ["appendfile"] = appendfile or syn_io_append or append_file,
+        ["makefolder"] = makefolder or make_folder or createfolder or create_folder,
+        -- // Environment Manipulation \--
+        ["hookfunction"] = hookfunction or hookfunc or detour_function,
+        ["hookmetamethod"] = hookmetamethod or hook_meta_method,
+        ["islclosure"] = islclosure or is_lclosure or isluaclosure,
+        ["iscclosure"] = iscclosure or is_cclosure,
+        ["newcclosure"] = newcclosure or new_cclosure,
+        ["cloneref"] = cloneref or clonereference,
+        ["getconnections"] = getconnections or get_connections or get_signal_cons,
+        ["getnamecallmethod"] = getnamecallmethod or get_namecall_method,
+        ["setnamecallmethod"] = setnamecallmethod or set_namecall_method,
+        -- // Instance Functions \--
+        ["getnilinstances"] = getnilinstances or get_nil_instances,
+        ["getproperties"] = getproperties or get_properties,
+        ["fireclickdetector"] = fireclickdetector or fire_click_detector,
+        ["gethiddenproperties"] = gethiddenproperties or get_hidden_properties or gethiddenprop or get_hidden_prop,
+        ["sethiddenproperties"] = sethiddenproperties or set_hidden_properties or sethiddenprop or set_hidden_prop,
+        ["getscripts"] = getrunningscripts or getscripts or get_running_scripts or get_scripts,
+        -- // Script Methods \--
+        ["getthreadcontext"] = getthreadcontext or get_thread_context or getthreadidentity or get_thread_identity,
+        ["setthreadcontext"] = setthreadcontext or set_thread_context or setthreadidentity or set_thread_identity,
+        ["getcallingscript"] = getcallingscript or get_calling_script,
+        ["getscriptclosure"] = getscriptclosure,
+        -- // Misc Functions \--
+        ["http_request"] = http_request or request or httprequest,
+        ["isluau"] = function()
+            return true
+        end,
+        ["writeclipboard"] = write_clipboard or writeclipboard or setclipboard or set_clipboard,
+        ["queue_on_teleport"] = queue_on_teleport or queueonteleport,
+        ["firesignal"] = fire_signal or firesignal
+    }
 
-		-- // IO Functions \--
-		["isfolder"] = isfolder or syn_isfolder or is_folder,
-		["isfile"] = isfile or syn_isfile or is_file,
-		["delfolder"] = delfolder or syn_delfolder or del_folder,
-		["delfile"] = delfile or syn_delfile or del_file,
-		["appendfile"] = appendfile or syn_io_append or append_file,
-		["makefolder"] = makefolder or make_folder or createfolder or create_folder,
+    for name, func in pairs(Functions) do
+        if typeof(func) ~= "function" then
+            table.insert(issues, name .. " is missing or not a function.")
+        end
+    end
 
-		-- // Environment Manipulation \--
-		["hookfunction"] = hookfunction or hookfunc or detour_function,
-		["hookmetamethod"] = hookmetamethod or hook_meta_method,
-		["islclosure"] = islclosure or is_lclosure or isluaclosure,
-		["iscclosure"] = iscclosure or is_cclosure,
-		["newcclosure"] = newcclosure or new_cclosure,
-		["cloneref"] = cloneref or clonereference,
-		["getconnections"] = getconnections or get_connections or get_signal_cons,
-		["getnamecallmethod"] = getnamecallmethod or get_namecall_method,
-		["setnamecallmethod"] = setnamecallmethod or set_namecall_method,
+    if #issues == 1 then
+        local message =
+            "[The Red Lake] Environment error detected:\n" ..
+            issues[1] .. "\nPlease contact the developer and paste the copied info."
+        if setclipboard then
+            setclipboard(message)
+        end
 
-		-- // Instance Functions \--
-		["getnilinstances"] = getnilinstances or get_nil_instances,
-		["getproperties"] = getproperties or get_properties,
-		["fireclickdetector"] = fireclickdetector or fire_click_detector,
-		["gethiddenproperties"] = gethiddenproperties or get_hidden_properties or gethiddenprop or get_hidden_prop,
-		["sethiddenproperties"] = sethiddenproperties or set_hidden_properties or sethiddenprop or set_hidden_prop,
-		["getscripts"] = getrunningscripts or getscripts or get_running_scripts or get_scripts,
-
-		-- // Script Methods \--
-		["getthreadcontext"] = getthreadcontext or get_thread_context or getthreadidentity or get_thread_identity,
-		["setthreadcontext"] = setthreadcontext or set_thread_context or setthreadidentity or set_thread_identity,
-		["getcallingscript"] = getcallingscript or get_calling_script,
-		["getscriptclosure"] = getscriptclosure,
-
-		-- // Misc Functions \--
-		["http_request"] = http_request or request or httprequest,
-		["isluau"] = function() return true end,
-		["writeclipboard"] = write_clipboard or writeclipboard or setclipboard or set_clipboard,
-		["queue_on_teleport"] = queue_on_teleport or queueonteleport,
-		["firesignal"] = fire_signal or firesignal,
-	}
-
-	for name, func in pairs(Functions) do
-		if typeof(func) ~= "function" then
-			table.insert(issues, name .. " is missing or not a function.")
-		end
-	end
-
-	if #issues == 1 then
-		local message = "[The Red Lake] Environment error detected:\n" .. issues[1] .. "\nPlease contact the developer and paste the copied info."
-		if setclipboard then setclipboard(message) end
-
-		Notify:Create({
-			Type = "Error",
-			Message = "Executor issue! Info copied for developer.",
-			Duration = 5
-		})
-	elseif #issues == 0 then
-		load()
-		getgenv().__LOADER_USED = true
-	end
+        Notify:Create(
+            {
+                Type = "Error",
+                Message = "Executor issue! Info copied for developer.",
+                Duration = 5
+            }
+        )
+    elseif #issues == 0 then
+        load()
+        getgenv().__LOADER_USED = true
+    end
 end
 
 checkExecutor()
